8d52161a9bab7bd728c22b52632ef51c151a475e
Michael <michael@michael-VirtualBox.(none)>
2014-10-05 18:05:40 -0700

    Tabs to spaces...

--- p1/Robot.cpp
+++ p1/Robot.cpp
@@ -1,113 +1,113 @@
 #include "Robot.h"
 
 Robot::Robot(char* fileName, char* error) {
-	sensory_error = atof(error);
-	buildGrid(fileName);
+    sensory_error = atof(error);
+    buildGrid(fileName);
 }
 
 void Robot::buildGrid(char* fileName) {
-	string line;
-	ifstream file;
-	file.open(fileName);	
-	while(getline(file, line)) {
-		vector<int> vec;				
-		stringstream ss(line);
-		string value;
-		while(ss >> value) {
-			vec.push_back(atoi(value.c_str()));			
-		}		
-		grid.push_back(vec);
-	}
-	file.close();
-	cout << endl;
+    string line;
+    ifstream file;
+    file.open(fileName);    
+    while(getline(file, line)) {
+        vector<int> vec;                
+        stringstream ss(line);
+        string value;
+        while(ss >> value) {
+            vec.push_back(atoi(value.c_str()));         
+        }       
+        grid.push_back(vec);
+    }
+    file.close();
+    cout << endl;
     printGrid();
     cout << endl;
-	int dimen = grid.size() * grid[0].size();
-	float matrix[dimen][dimen];
-	for(int i = 0; i < dimen; i++) {
-		int x = i / grid[0].size();
-		int y = i % grid[0].size();
-		int cell_value = grid[x][y];
-		int neighbors = getBitsDifference(cell_value, 15);
-		fprintf(stdout, "cell[%d][%d] has %d nieghbors\n", x, y, neighbors);
-		string walls = intToBinary(cell_value);		
-	
-		// N
-		if(walls[0] == '0') {
-			matrix[grid[0].size() - i][i] = 1 / neighbors;
-		}
-		
-		// S
-		if(walls[1] == '0') {
-			matrix[grid[0].size() + i][i] = 1 / neighbors;
-		}
-		
-		// W
-		if(walls[2] == '0') {
-			matrix[i - 1][i] = 1 / neighbors;
-		}
+    int dimen = grid.size() * grid[0].size();
+    float matrix[dimen][dimen];
+    for(int i = 0; i < dimen; i++) {
+        int x = i / grid[0].size();
+        int y = i % grid[0].size();
+        int cell_value = grid[x][y];
+        int neighbors = getBitsDifference(cell_value, 15);
+        fprintf(stdout, "cell[%d][%d] has %d nieghbors\n", x, y, neighbors);
+        string walls = intToBinary(cell_value);     
+    
+        // N
+        if(walls[0] == '0') {
+            matrix[grid[0].size() - i][i] = 1 / neighbors;
+        }
+        
+        // S
+        if(walls[1] == '0') {
+            matrix[grid[0].size() + i][i] = 1 / neighbors;
+        }
+        
+        // W
+        if(walls[2] == '0') {
+            matrix[i - 1][i] = 1 / neighbors;
+        }
 
-		// E
-		if(walls[3] == '0') {
-			matrix[i + 1][i] = 1 / neighbors;
-		}
+        // E
+        if(walls[3] == '0') {
+            matrix[i + 1][i] = 1 / neighbors;
+        }
 
-		// for(int q = 0; q < dimen; q++) {
-		// 	cout << matrix[q][i] << " ";
-		// }
-		// cout << endl;
-	}		
+        // for(int q = 0; q < dimen; q++) {
+        //  cout << matrix[q][i] << " ";
+        // }
+        // cout << endl;
+    }       
 }
 
 void Robot::printGrid() {
-	for(int i = 0; i < grid.size(); i++) {
-		for(int j = 0; j < grid[i].size(); j++) {
-			fprintf(stdout,"%d ", grid[i][j]);
-		}
-		cout << endl;
-	}
+    for(int i = 0; i < grid.size(); i++) {
+        for(int j = 0; j < grid[i].size(); j++) {
+            fprintf(stdout,"%d ", grid[i][j]);
+        }
+        cout << endl;
+    }
 }
 
 void Robot::addObservation(string pObservation) {
-	observations.push_back(pObservation);
+    observations.push_back(pObservation);
 }
 
 int Robot::getBitsDifference(int a, int b) {
-	int x = a^b;
-	unsigned int y;
-	for(y = 0; x; y++) {
-		x &= x - 1;
-	}
-	return y;
+    int x = a^b;
+    unsigned int y;
+    for(y = 0; x; y++) {
+        x &= x - 1;
+    }
+    return y;
 }
 
 int Robot::NSWE(string r) {
-	int result = 0;
-	if(inString(r, 'N')) result += 8;
-	if(inString(r, 'S')) result += 4;
-	if(inString(r, 'W')) result += 2;
-	if(inString(r, 'E')) result += 1;	
-	return result;
+    int result = 0;
+    if(inString(r, 'N')) result += 8;
+    if(inString(r, 'S')) result += 4;
+    if(inString(r, 'W')) result += 2;
+    if(inString(r, 'E')) result += 1;   
+    return result;
 }
 
 bool Robot::inString(string s, char c) {
-	for(int i = 0; i < s.size(); i++) {
-		if(s[i] == c) {
-			return true;
-		}
-	}
-	return false;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == c) {
+            return true;
+        }
+    }
+    return false;
 }
 
 string Robot::intToBinary(int n) {
-	bitset<4> bits(n);		
-	return bits.to_string();
+    bitset<4> bits(n);      
+    return bits.to_string();
 }
 
 vector<float> Robot::calculate_diff_values(float e) {
-	vector<float> result;
-	for(int i = 0; i <5; i++) {
-		result.push_back(pow(e, i) * pow(1 - e, 4 - i));
-	}
-	return result;
+    vector<float> result;
+    for(int i = 0; i <5; i++) {
+        result.push_back(pow(e, i) * pow(1 - e, 4 - i));
+    }
+    return result;
 }
--- p1/Robot.h
+++ p1/Robot.h
@@ -16,63 +16,63 @@ using namespace std;
 class Robot {
 public:
 
-	// Robot constructor
-	Robot(char*, char*);
+    // Robot constructor
+    Robot(char*, char*);
 
-	// Reads file and populates grid vector	
-	void buildGrid(char*);
+    // Reads file and populates grid vector 
+    void buildGrid(char*);
 
-	// Displays the grid for debugging
-	void printGrid();
+    // Displays the grid for debugging
+    void printGrid();
 
-	// Observation helper to add values
-	void addObservation(string);
+    // Observation helper to add values
+    void addObservation(string);
 
-	float getError() { return sensory_error; }
+    float getError() { return sensory_error; }
 
-	// Calculates number of differing binary bits between ints
-	int getBitsDifference(int, int);
+    // Calculates number of differing binary bits between ints
+    int getBitsDifference(int, int);
 
-	// Converts observation string to integer value
-	int NSWE(string);
+    // Converts observation string to integer value
+    int NSWE(string);
 
-	// Helper that finds characters in strings
-	bool inString(string, char);
+    // Helper that finds characters in strings
+    bool inString(string, char);
 
-	// Converts int to binary string
-	string intToBinary(int);
+    // Converts int to binary string
+    string intToBinary(int);
 
-	// Calculate difference values
-	vector<float> calculate_diff_values(float);
+    // Calculate difference values
+    vector<float> calculate_diff_values(float);
 
 private:
 
-	// Holds robots world grid values
-	vector<vector<int> > grid;
+    // Holds robots world grid values
+    vector<vector<int> > grid;
 
-	// Sensory error of inputs
-	float sensory_error;
+    // Sensory error of inputs
+    float sensory_error;
 
-	// All observations specified by the user at runtime
-	vector<string> observations;
+    // All observations specified by the user at runtime
+    vector<string> observations;
 
-	// Need a more descriptive name
-	float diff_values[5];
+    // Need a more descriptive name
+    float diff_values[5];
 
 };
 
-int main(int argc, char* argv[]) {	
-	Robot robot(argv[1], argv[2]);
-	for(int i = 3; i < argc; i++) {
-		robot.addObservation(argv[i]);
-	}
-	// cout << robot.getBitsDifference(15, 0) << endl;
-	// cout << robot.NSWE("NS") << endl;
-	// cout << robot.intToBinary(11) << endl;    		
-	// vector<float> diffs = robot.calculate_diff_values(robot.getError());
-	// for(int i = 0; i < diffs.size(); i++) {
-	// 	fprintf(stdout, "[%d]: %f\n", i, diffs[i]);
-	// }
+int main(int argc, char* argv[]) {  
+    Robot robot(argv[1], argv[2]);
+    for(int i = 3; i < argc; i++) {
+        robot.addObservation(argv[i]);
+    }
+    // cout << robot.getBitsDifference(15, 0) << endl;
+    // cout << robot.NSWE("NS") << endl;
+    // cout << robot.intToBinary(11) << endl;           
+    // vector<float> diffs = robot.calculate_diff_values(robot.getError());
+    // for(int i = 0; i < diffs.size(); i++) {
+    //  fprintf(stdout, "[%d]: %f\n", i, diffs[i]);
+    // }
 }
 
 #endif
