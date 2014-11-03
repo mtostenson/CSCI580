3eb3e9f71d0614c318dfba9552c872b8941f3271
mt523 <mtostenson@mail.csuchico.edu>
2014-11-03 10:26:42 -0800

    Almost finished

--- hw7/hw7/hw7.cpp
+++ hw7/hw7/hw7.cpp
@@ -3,22 +3,85 @@
 
 #include "stdafx.h"
 
+#define ALPHA 0.01f
+
+// Helper methods
+double dotProduct(vector<int>& a, vector<double>& b)
+{
+    double result = 0;
+    for(int i = 0; i < a.size(); i++)
+    {
+        result += a[i]* b[i];
+    }
+    return result;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    vector<vector<int> > input;
+    vector<int> class_labels;
+    ifstream file;
+    string line;
+    stringstream ss;
 
     // Read file 1
+    file.open(argv[1]);
+    while(getline(file, line))
+    {
+        ss << line;
+        vector<int> row;
+        string value;
+        while(ss >> value)
+        {
+            row.push_back(atoi(value.c_str()));
+        }
+        input.push_back(row);
+        ss.clear();
+    }
+    file.close();
 
     // Read file 2
+    file.open(argv[2]);
+    while(getline(file, line))
+    {
+        class_labels.push_back(atoi(line.c_str()));
+    }
+    ss.clear();
+    file.close();
 
     // Add dummy attribute
+    for(int i = 0; i < input.size(); i++)
+    {
+        input[i].insert(input[i].begin(), 1);
+    }
 
     // Set initial weights
+    vector<vector<double> > w(input.size());
+    for(int i = 0; i < input.size(); i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            w[i].push_back(1);
+        }
+    }
 
-    // Loop 10 times
+    // Loop 10 times    
+    vector<double> h(input.size());
+    for (int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < input.size(); j++)
+        {
+            h[j] = 1 / (1 + exp(-dotProduct(input[j], w[j])));
+
+            for(int k = 0; k < input[0].size(); k++)
+            {
+                w[j][k] = w[j][k] + ALPHA * (input[j].back() - h[j]) * h[j] * (1 - h[j]) * input[j][k];
+            }
+        }       
+        
+    }
 
     // Print output
 
 	return 0;
 }
-
--- hw7/hw7/stdafx.h
+++ hw7/hw7/stdafx.h
@@ -13,6 +13,8 @@
 #include <stdio.h>
 #include <iomanip>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
