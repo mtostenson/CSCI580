5bfec7baec0d4ddd25232fdc51e4659fa4e237bd
mt523 <mtostenson@mail.csuchico.edu>
2014-11-03 08:19:10 -0800

    hw7 initial commit

---
+++ hw7/hw7/hw7.cpp
@@ -0,0 +1,24 @@
+// hw7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+    // Read file 1
+
+    // Read file 2
+
+    // Add dummy attribute
+
+    // Set initial weights
+
+    // Loop 10 times
+
+    // Print output
+
+	return 0;
+}
+
